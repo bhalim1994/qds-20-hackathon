f0393f152ec63a7ffcf2f954d788ad100465d49a
Haejoon Choi <haejoonchoi@outlook.com>
2020-01-11 13:21:58 -0800

    made a gitignore file

--- app.js
+++ app.js
@@ -198,3 +198,96 @@ app.listen(port, function () {
 });
 
 module.exports = app;
+
+const path = require('path');
+
+const viewsRouter = require('./routes/views');
+
+const getDate = require('./ajax/getDate');
+const getBillboard = require('./ajax/getBillboard');
+const getBillboard2 = require('./ajax/getBillboard2');
+
+
+app.use('/', viewsRouter);
+app.use('/public', express.static('public'));
+app.use('/data', express.static('data'));
+
+app.get('/ajax-getDate', getDate);
+app.get('/ajax-getBillboard', getBillboard);
+app.get('/ajax-getBillboard2', getBillboard2);
+
+
+// for page not found (i.e., 404)
+app.use(function (req, res, next) {
+  res.status(404).send("<html><head><title>Page not found!</title></head><body><p>Nothing here.</p></body></html>");
+});
+
+// RUN SERVER
+let port = 9000;
+app.listen(port, function () {
+    console.log('Ajax lab listening on port ' + port + '!');
+});
+
+module.exports = app;
+
+const path = require('path');
+
+const viewsRouter = require('./routes/views');
+
+const getDate = require('./ajax/getDate');
+const getBillboard = require('./ajax/getBillboard');
+const getBillboard2 = require('./ajax/getBillboard2');
+
+
+app.use('/', viewsRouter);
+app.use('/public', express.static('public'));
+app.use('/data', express.static('data'));
+
+app.get('/ajax-getDate', getDate);
+app.get('/ajax-getBillboard', getBillboard);
+app.get('/ajax-getBillboard2', getBillboard2);
+
+
+// for page not found (i.e., 404)
+app.use(function (req, res, next) {
+  res.status(404).send("<html><head><title>Page not found!</title></head><body><p>Nothing here.</p></body></html>");
+});
+
+// RUN SERVER
+let port = 9000;
+app.listen(port, function () {
+    console.log('Ajax lab listening on port ' + port + '!');
+});
+
+module.exports = app;
+
+const path = require('path');
+
+const viewsRouter = require('./routes/views');
+
+const getDate = require('./ajax/getDate');
+const getBillboard = require('./ajax/getBillboard');
+const getBillboard2 = require('./ajax/getBillboard2');
+
+
+app.use('/', viewsRouter);
+app.use('/public', express.static('public'));
+app.use('/data', express.static('data'));
+
+app.get('/ajax-getDate', getDate);
+app.get('/ajax-getBillboard', getBillboard);
+app.get('/ajax-getBillboard2', getBillboard2);
+
+
+// for page not found (i.e., 404)
+app.use(function (req, res, next) {
+  res.status(404).send("<html><head><title>Page not found!</title></head><body><p>Nothing here.</p></body></html>");
+});
+
+// RUN SERVER
+let port = 9000;
+app.listen(port, function () {
+    console.log('Ajax lab listening on port ' + port + '!');
+});
+
+module.exports = app;
